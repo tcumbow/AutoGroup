c3a3fb74b05df38b86959a56e7a2e7ff1650f606
Tom Cumbow <tcumbow@icloud.com>
2020-12-02 17:16:11 -0500

    folderized

---
+++ Sync.ps1
@@ -0,0 +1,5 @@
+unison .\AutoGroup A:\AutoGroup -auto -batch
+pause
+unison .\AutoGroup R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoGroup -auto -batch
+pause
+unison .\AutoGroup "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoGroup" -auto -batch
