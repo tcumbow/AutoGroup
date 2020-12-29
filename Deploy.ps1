2ee219f1d91a569f23a2f85a23a485db47763aa6
Tom Cumbow <tcumbow@icloud.com>
2020-12-29 18:19:26 -0500

    new deploy model

---
+++ Deploy.ps1
@@ -0,0 +1,2 @@
+robocopy .\AutoGroup R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoGroup /mir
+robocopy .\AutoGroup "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoGroup" /mir
--- Sync.ps1
+++
@@ -1,5 +0,0 @@
-unison .\AutoGroup A:\AutoGroup -auto -batch
-pause
-unison .\AutoGroup R:\Documents\Jenny_ESO_Live_repo\Jenny_ESO_Live\AddOns\AutoGroup -auto -batch
-pause
-unison .\AutoGroup "\\samantha-gaming\Users\saman\OneDrive\Documents\Elder Scrolls Online\live\AddOns\AutoGroup" -auto -batch
